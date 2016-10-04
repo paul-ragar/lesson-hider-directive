abadd68e66cb5a7a45ca7d70088d76c8310bf6c5
Paul Ragar <pragar@Pauls-MacBook-Pro.local>
2016-10-04 16:36:39 -0600

    To be continued. Need to add delete button function

---
+++ lessonCtrl.js
@@ -0,0 +1,13 @@
+angular.module('directivePractice').controller('lessonCtrl', function($scope) {
+
+  $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+
+  $scope.announceDay = function(lesson, day) {
+    if (!day) {
+      alert(lesson + ' is not on the schedule for this week.');
+    }
+    else {
+      alert(lesson + ' is active on ' + day + '.');
+    }
+  }
+});
---
+++ lessonHider.js
@@ -0,0 +1,32 @@
+angular.module('directivePractice').directive('lessonHider', function() {
+
+  return {
+    templateUrl: 'lessonHider.html',
+    restrict: 'E',
+    scope: {
+      lesson: '=',
+      dayAlert: '&'
+    },
+    controller: function($scope, lessonService) {
+      $scope.getSchedule = lessonService.getSchedule();
+
+    },
+    link: function(scope, element, attributes) {
+      scope.getSchedule.then(function(response) {
+        scope.schedule = response.data;
+
+
+        scope.schedule.forEach(function( scheduleDay ) {
+          if (scheduleDay.lesson === scope.lesson) {
+            scope.lessonDay = scheduleDay.weekday;
+            scope.checked = true;
+            element.css('text-decoration', 'line-through');
+            return;
+          }
+        });
+      });
+
+    }
+  }
+
+});
